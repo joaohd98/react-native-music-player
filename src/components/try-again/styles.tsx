7861f344fa13f5186ed71341df8f1aec2c8802d6
João Damazio <joaohd98@gmail.com>
2020-09-29 14:56:53 -0300

    Finish styles tryAgain

--- src/components/try-again/styles.tsx
+++ src/components/try-again/styles.tsx
@@ -1,19 +1,34 @@
 import styled from 'styled-components/native';
+import {CustomColors} from "../../theme/colors";
+import {CustomFonts} from "../../theme/fonts";
 
 export const TryAgainStyle  = {
   ContainerView: styled.View`
-
+    padding: 20px;
+    align-items: center;
   `,
   FirstLineText: styled.Text`
-
+    font-size: 16px;
+    font-family: ${CustomFonts.circularStdBold};
+    color: ${CustomColors.white};
+    text-align: center;
   `,
   SecondsLineText: styled.Text`
-
+    font-size: 14px;
+    font-family: ${CustomFonts.circularStdMedium};
+    color: ${CustomColors.white};
+    text-align: center;
   `,
   TouchableButton: styled.TouchableOpacity`
-
+     margin-top: 25px;
+     background-color: ${CustomColors.spotifyColor};
+     width: 120px;
+     padding: 10px 5px;
+     border-radius: 20px;
   `,
   TouchableText: styled.Text`
-
+    color: white;
+    text-align: center;
+    font-family: ${CustomFonts.circularStdBold};
   `
 }
